771072ecbdf1e50555920197dc3fffcffacea61e
madyler <madyler@mail.ru>
2022-02-10 16:06:03 +0300

    divided into components

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return <header className="header">
+        <img
+            alt={''}
+            src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrPotIEiUTbNLUNoJE5gAdjdirbVcFSvLFvg&usqp=CAU'}/>
+    </header>
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img alt={''} src={'https://smapse.ru/storage/2018/03/academy-img-1.jpg'}/>
+        </div>
+        <div>
+            Avatar + Description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
